49f60118b85aec096cbe5c9907e793fb67c1121c
Deepak Mallubhotla <dmallubhotla+github@gmail.com>
2026-05-19 10:13:46 -0500

    Release 0.1.2

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
       kestOverlay = final: _prev: {
         kest = final.buildGoApplication rec {
           pname = "kest";
-          version = "0.1.1";
+          version = "0.1.2";
           src = ./.;
           modules = ./gomod2nix.toml;
           nativeBuildInputs = [ final.installShellFiles ];
